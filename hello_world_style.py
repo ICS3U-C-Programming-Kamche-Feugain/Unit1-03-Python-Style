2577804d058d1ed1143e445ba21c339cd9c2a881
Kamche Feugain Sofo <kamche.feugain-sofo@stu.ocsb.ca>
2026-09-14 22:58:06 +0000

    First commitof hello_world_updated.py

---
+++ hello_world_style.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3\
+# Created By: Your Kamche
+# Date: September 14, 2026
+# This program displays a greeting and contains proper formatting.
+def main():
+    print("Hello. Kamche")
+    print("Programming is so cool!")
+
+
+if __name__ == "__main__":
+
+    main()
